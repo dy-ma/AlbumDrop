a550acdbe5a53c798d4d22f8b90c8a61dc14bd93
Dylan Ang <73457638+stack-dylan@users.noreply.github.com>
2025-03-14 12:59:21 -0700

    add credentials to ses client

--- src/lib/ses.ts
+++ src/lib/ses.ts
@@ -2,7 +2,17 @@ import { SendEmailCommand, SESClient } from "@aws-sdk/client-ses";
 
 const REGION = "us-west-1" // n california
 
-export const sesClient = new SESClient({ region: REGION });
+if (!process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_KEY) {
+  throw new Error("Missing AWS Credentials in Environment Variables")
+}
+
+export const sesClient = new SESClient({
+  region: REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_KEY!
+  }
+});
 
 export async function sendEmail({ from, to, subject, body }: { from?: string, to: string, subject: string, body: string }) {
   await sesClient.send(new SendEmailCommand({
