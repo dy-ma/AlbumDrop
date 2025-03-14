e2d2e09f698baa8b8b5a8eabbced610cfd06bd26
Dylan Ang <73457638+stack-dylan@users.noreply.github.com>
2025-03-14 13:04:11 -0700

    Modify env variable names to avoid reserved names

--- src/lib/ses.ts
+++ src/lib/ses.ts
@@ -1,16 +1,15 @@
 import { SendEmailCommand, SESClient } from "@aws-sdk/client-ses";
 
-const REGION = "us-west-1" // n california
 
-if (!process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_KEY) {
+if (!process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY) {
   throw new Error("Missing AWS Credentials in Environment Variables")
 }
 
 export const sesClient = new SESClient({
-  region: REGION,
+  region: process.env.AWS_REGION || "us-west-1",
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_KEY!
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!
   }
 });
 
